0370409cf8e0fcb35d6d50429c4cad215c558fe2
CHALAMSETTY NIVAS KRISHNA <chalamsettynivas@gmail.com>
2026-07-03 14:00:10 +0530

    Time: 0 ms (100%), Space: 43 MB (69.88%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,19 @@
+class Solution{
+    private void dfs(TreeNode root, List<Integer> ans){
+        if(root == null)
+            return;
+
+        ans.add(root.val);
+        dfs(root.left, ans);
+        dfs(root.right, ans);
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
+        dfs(root, ans);
+        return ans;
+    }
+
+
+
+}
