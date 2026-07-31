17100bc9fb0659cf94a3124d1d140301ddded29b
CHALAMSETTY NIVAS KRISHNA <chalamsettynivas@gmail.com>
2026-07-31 17:08:57 +0530

    Time: 1 ms (23.53%), Space: 49 MB (5.13%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+       
+        reverse(s, 0, s.length-1);
+    }
+    private void reverse(char[] s, int i, int j){
+        if(i >= j){
+            return;
+        }
+
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        reverse(s,i+1, j-1);
+    }
+}
